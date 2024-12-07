fc5a001a2ced99a57e1b07231a654e671f1570ee
Aleksandar <simic.aleksandar.sd@gmail.com>
2024-12-07 22:11:00 +0100

    Added appwrite config

---
+++ react_native_crash_course/lib/appwrite.js
@@ -0,0 +1,8 @@
+export const appWriteConfig = {
+    endpoint : "https://cloud.appwrite.io/v1",
+    platform : 'com.jsm.aora',
+    projectId : '6754b66f001eb44fddc3',
+    databaseId: '6754b94d003bc379b984',
+    userCollectionId : '6754b965003747655500',
+    videoCollectionId : '6754b9bc0012fa2d2758'
+}
